8d140e223fec518bbb140f86014d92785b66080d
daivp <daivp@aoi-sys.vn>
2016-03-11 18:11:53 +0700

    commit 1

--- MyApplication2/app/src/main/java/com/phuocdai/myapplication/Splash.java
+++ MyApplication2/app/src/main/java/com/phuocdai/myapplication/Splash.java
@@ -1,17 +1,35 @@
 package com.phuocdai.myapplication;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class Splash extends ActionBarActivity {
+public class Splash extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        final Intent intent = new Intent(Splash.this, MainActivity.class);
+        Thread splashThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (this){
+                    try {
+                        wait(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } finally {
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+            }
+        });
+        splashThread.start();
     }
 
 
